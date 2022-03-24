62d45a9a592cead6e3796cac48b1eabc9fafc9ed
Sammy Guttman <1449@holbertonschool.com>
2022-03-24 15:35:21 -0500

    change logic to move camera

--- 0x06-unity-assets_ui/Assets/Scripts/CameraController.cs
+++ 0x06-unity-assets_ui/Assets/Scripts/CameraController.cs
@@ -2,44 +2,59 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public float panSpeed = 0f;
-
-    private Vector3 mouseOrigin;
-    private bool isPanning;
+    public Transform pivot;
+    protected Vector3 _LocalRotation;
+    public float MouseSensitivity = 4f;
+    public bool CameraDisabled = false;
+    public bool isInverted = false;
+    protected float _CameraDistance = 10f;
+    public float ScrollSensitvity = 2f;
+    public float OrbitDampening = 10f;
+    public float ScrollDampening = 6f;
     
-    public Vector2 MouseInput()
+    void Start()
     {
-        float x = Input.GetAxis("Mouse X");
-        float y = Input.GetAxis("Mouse Y");
-
-        return new Vector2(x, y);
+        Cursor.visible = false; // set Cursor invisible
+        transform.SetParent(pivot, true);
+        isInverted = PlayerPrefs.GetInt("invertYAxis") == 1 ? true : false;
     }
 
-    // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        if (Input.GetMouseButtonDown(1)) // 0: left and 1: right
+        if(!CameraDisabled)
         {
-            mouseOrigin = Input.mousePosition;
-            isPanning = true;
-        }
-
-        // cancel on button release
-        if (!Input.GetMouseButton(1))
-        {
-            isPanning = false;
-        }
-
-        //move camera on X & Y
-        if (isPanning)
-        {
-            Vector2 input = MouseInput();
-            Debug.Log(Time.deltaTime);
-            transform.Rotate(input.y * panSpeed * Time.deltaTime, input.x * panSpeed * Time.deltaTime, 0);
-            /* if (input.y > 0)
+            //Rotation of the Camera based on Mouse Coordinates
+            if(Input.GetAxis("Mouse X") != 0 || Input.GetAxis("Mouse Y") != 0)
             {
-                transform.Rotate(Vector3.up * input.y * panSpeed * Time.deltaTime);
-            } */
+                _LocalRotation.x += Input.GetAxis("Mouse X") * MouseSensitivity;
+                if (!isInverted)
+                    _LocalRotation.y -= Input.GetAxis("Mouse Y") * MouseSensitivity;
+                else
+                    _LocalRotation.y += Input.GetAxis("Mouse Y") * MouseSensitivity;
+
+                //Clamp the y Rotation to horizon and not flipping over at the top
+                _LocalRotation.y = Mathf.Clamp(_LocalRotation.y, -20f, 90f);
+            }
+            //Zooming Input from our Mouse Scroll Wheel
+            if(Input.GetAxis("Mouse ScrollWheel") != 0f)
+            {
+                float ScrollAmount = Input.GetAxis("Mouse ScrollWheel") * ScrollSensitvity;
+
+                ScrollAmount *= (_CameraDistance * 0.3f);
+
+                _CameraDistance += ScrollAmount * -1f;
+
+                _CameraDistance = Mathf.Clamp(_CameraDistance, 3f, 20f);
+            }
+        }
+
+        //Actual Camera Rig Transformations
+        Quaternion QT = Quaternion.Euler(_LocalRotation.y, _LocalRotation.x, 0);
+        pivot.rotation = Quaternion.Lerp(pivot.rotation, QT, Time.deltaTime * OrbitDampening);
+
+        if(transform.localPosition.z != this._CameraDistance * -1f)
+        {
+            transform.localPosition = new Vector3(0f, 2f, Mathf.Lerp(transform.localPosition.z, _CameraDistance * -1f, Time.deltaTime * ScrollDampening));
         }
 
     }
